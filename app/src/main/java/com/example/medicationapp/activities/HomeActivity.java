e036ce2609aabb11c9ea2b0cef7b03baf10f7563
puneet <puneet151293@gmail.com>
2019-08-30 12:58:49 -0500

    Added Navigation Bar

--- app/src/main/java/com/example/medicationapp/activities/HomeActivity.java
+++ app/src/main/java/com/example/medicationapp/activities/HomeActivity.java
@@ -1,18 +1,27 @@
 package com.example.medicationapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.medicationapp.R;
 import com.example.medicationapp.database.DataBaseHelper;
+import com.example.medicationapp.fragments.AddMedicationFragment;
 import com.example.medicationapp.fragments.MainFragment;
 import com.example.medicationapp.fragments.MedicationDetailsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity implements MedicationDetailsFragment.OnMedicationDetailsFragmentListener, MainFragment.OnMainFragmentInteractionListener {
+public class HomeActivity extends AppCompatActivity implements MedicationDetailsFragment.OnMedicationDetailsFragmentListener, MainFragment.OnMainFragmentInteractionListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     DataBaseHelper db;
 
@@ -34,6 +43,30 @@ public class HomeActivity extends AppCompatActivity implements MedicationDetails
             mainFragment = MainFragment.newInstance();
             fm.beginTransaction().add(R.id.container_main, mainFragment).commit();
         }
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        //loadFragment(new MainFragment());
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,R.id.navigation_add_medication)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_main,fragment).commit();
+
+            return true;
+        }
+
+        return false;
     }
 
     @Override
@@ -44,5 +77,22 @@ public class HomeActivity extends AppCompatActivity implements MedicationDetails
     @Override
     public void onMedicationDetailsFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment =null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new MainFragment();
+                break;
+            case R.id.navigation_add_medication:
+                fragment = new AddMedicationFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
